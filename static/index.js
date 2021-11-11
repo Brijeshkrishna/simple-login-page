d20bafc3f1b01f794b3513eb60a06469bfe14dc0
brijesh <brijeshkrishnaga@gmail.com>
2021-11-11 22:33:49 +0530

    add readme , package and some testing in index.js file

--- static/index.js
+++ static/index.js
@@ -1,9 +1,14 @@
-const $ = document;
-
-function click_b1() {
-  const form_1 = $.getElementById("form_1");
-  //form_1.hidden = true;
-  const input1 = $.getElementById("dt_1").innerText
-  const input2 = $.getElementById("tb1").innerText
-    $.write(input1 + " " + input2);
+function Name (name,age,edu) {
+    this.name = name;
+    this.age = age;
+    this.edu =edu;
+    
+    this.get = (name) =>{
+        return this[name]
+    }
 }
+
+
+var d1  = new 
+
+console.log(d1.toISOString());
