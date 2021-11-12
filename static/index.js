c54435464f45b016e83ae8926be8fe6a1a4d35bf
brijesh <brijeshkrishnaga@gmail.com>
2021-11-12 19:11:25 +0530

    add radio button

--- static/index.js
+++ static/index.js
@@ -1,7 +1,20 @@
-function bt1Click(id){
-    const bt1_vlaue = document.getElementById(id);
+function fnt1()
+{
+    const rd1 = document.getElementById("rd1");
+    const rd2 = document.getElementById("rd2");
 
 
-    bt1_vlaue.value = ""
-    
+    if (rd1.checked) {
+        alert(rd1.name);
+        rd1.checked = 0
+
+    }
+    else if (rd2.checked) {
+        alert(rd2.name);
+        rd2.checked = 0
+
+    }
+    else{
+        alert("select one plz..")
+    }
 }
